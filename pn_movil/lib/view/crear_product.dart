aed868f512a2059cf78b06591393d113970d90d0
Tibiie <yuraniecheverri8@gmail.com>
2024-11-13 21:57:02 -0500

    algo feo

--- pn_movil/lib/view/crear_product.dart
+++ pn_movil/lib/view/crear_product.dart
@@ -1,39 +1,143 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:pn_movil/widgets/drawer.dart';
 import 'package:pn_movil/widgets/navbar.dart';
 
-class Crearproduct extends StatelessWidget {
+class Crearproduct extends StatefulWidget {
   const Crearproduct({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: Navbar(),
-      drawer: CustomDrawer(),
-      body: CrearProductBody(),
-    );
-  }
+  _CrearproductState createState() => _CrearproductState();
 }
 
-class CrearProductBody extends StatelessWidget {
-  const CrearProductBody({super.key});
+class _CrearproductState extends State<Crearproduct> {
+  XFile? _image;
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+
+  Future<void> _pickImage() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? pickedImage =
+        await picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _image = pickedImage;
+    });
+  }
+
+  @override
+  void dispose() {
+    // Libera el controlador cuando ya no sea necesario
+    _titleController.dispose();
+    _descriptionController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: NetworkImage(
-              'https://i.pinimg.com/736x/71/7d/ce/717dce3d21e998822a3ca37065b932d3.jpg'),
-        ),
-      ),
-      child: Center(
-        child: Text(
-          'Crearproduct',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: const Color.fromARGB(255, 223, 87, 87),
+    return Scaffold(
+      appBar: const Navbar(),
+      drawer: const CustomDrawer(),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 80),
+                GestureDetector(
+                  onTap: _pickImage,
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      CircleAvatar(
+                        radius: 75,
+                        backgroundColor: Colors.grey[200],
+                        backgroundImage: _image != null
+                            ? FileImage(File(_image!.path))
+                            : null,
+                      ),
+                      if (_image == null)
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            const Icon(
+                              Icons.image,
+                              size: 50,
+                              color: Colors.grey,
+                            ),
+                            const SizedBox(height: 5),
+                            const Text(
+                              'Subir Imagen',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: _titleController,
+                  decoration: InputDecoration(
+                    labelText: 'Título del Producto',
+                    hintText: 'Escribe el título del producto...',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey[100],
+                    prefixIcon: const Icon(Icons.title),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: _descriptionController,
+                  maxLines: 4,
+                  decoration: InputDecoration(
+                    labelText: 'Descripción del Producto',
+                    hintText: 'Escribe una breve descripción...',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey[100],
+                    prefixIcon: const Icon(Icons.description),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: () {
+                    String title = _titleController.text;
+                    String description = _descriptionController.text;
+                    print('Título: $title');
+                    print('Descripción: $description');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 15, horizontal: 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    backgroundColor: Color.fromARGB(255, 90, 136, 204),
+                  ),
+                  child: const Text(
+                    'Guardar Producto',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+              ],
+            ),
           ),
         ),
       ),
--- pn_movil/macos/Flutter/GeneratedPluginRegistrant.swift
+++ pn_movil/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import file_selector_macos
 import google_sign_in_ios
 import local_auth_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   FLALocalAuthPlugin.register(with: registry.registrar(forPlugin: "FLALocalAuthPlugin"))
 }
