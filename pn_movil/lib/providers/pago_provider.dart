84bf88ac9b9987cffeb7f1b794d08f6cb0cf95c3
Tibiie <yuraniecheverri8@gmail.com>
2024-12-10 11:52:32 -0500

    Eliminar pago listo

--- pn_movil/lib/providers/pago_provider.dart
+++ pn_movil/lib/providers/pago_provider.dart
@@ -12,6 +12,7 @@ class PagoProvider extends ChangeNotifier {
   PagoProvider(this._apiClient);
 
   List<Map<String, dynamic>> _pago = [];
+  late Map<String, dynamic> _compra;
 
   bool _isLoading = false;
 
@@ -120,68 +121,16 @@ class PagoProvider extends ChangeNotifier {
       _isLoading = true;
       notifyListeners();
 
-      // Imprimir toda la lista de pagos para depuración
-      print('Lista de pagos: $_pago');
+      await searchCompra(context, pagoId);
 
-      // Buscar la compra que contiene el pago
-      final compra = _pago.firstWhere(
-        (c) => c['pago'] != null && c['pago']['idPago'] == pagoId,
-        orElse: () {
-          print('Compra no encontrada para el pago con ID: $pagoId');
-          return {}; // Retorna null si no encuentra la compra
-        },
-      );
+      final inventarioId = _compra['productoCompras'][0]
+          ['productoCompraInventory'][0]['inventory']['idInventory'];
+      print('ID del inventario: $inventarioId');
 
-      if (compra == null) {
-        throw Exception('Compra asociada al pago no encontrada');
-      }
-
-      // Imprimir la compra encontrada
-      print('Compra encontrada: $compra');
-
-      // Validar que 'productoCompras' esté presente y no esté vacío
-      final productoCompras = compra['productoCompras'];
-      if (productoCompras == null || productoCompras.isEmpty) {
-        print('productoCompras: $productoCompras');
-        throw Exception('ProductoCompras no disponible o vacío');
-      }
-
-      // Imprimir los productos de compra
-      print('ProductoCompras: $productoCompras');
-
-      // Acceder al primer elemento de 'productoCompras'
-      final productoCompra = productoCompras[0];
-
-      // Validar que 'productoCompraInventory' esté presente y no esté vacío
-      final productoCompraInventory = productoCompra['productoCompraInventory'];
-      if (productoCompraInventory == null || productoCompraInventory.isEmpty) {
-        print('productoCompraInventory: $productoCompraInventory');
-        throw Exception('ProductoCompraInventory no disponible o vacío');
-      }
-
-      // Imprimir el inventario del producto de compra
-      print('ProductoCompraInventory: $productoCompraInventory');
-
-      // Acceder al primer elemento de 'productoCompraInventory'
-      final inventory = productoCompraInventory[0]['inventory'];
-
-      // Validar que 'inventory' no sea null y que contenga 'idInventory'
-      if (inventory == null || inventory['idInventory'] == null) {
-        print('inventory: $inventory');
-        throw Exception(
-            'Inventory no disponible o el campo idInventory es nulo');
-      }
-
-      // Obtener idInventory como entero
-      final idInventory = inventory['idInventory'];
-      final inventarioId = int.tryParse(idInventory.toString()) ?? 0;
-
-      print('ID del Inventario: $inventarioId');
-
-      // Construir la URL con el pagoId y el idInventario como query
+      //   // Construir la URL con el pagoId y el idInventario como query
       final url = '/api/v1/pago/$pagoId?idInventario=$inventarioId';
 
-      // Realizar la petición para eliminar el pago
+      //   // Realizar la petición para eliminar el pago
       final response = await _apiClient.delete(url);
 
       if (response.statusCode == 200 || response.statusCode == 204) {
@@ -201,6 +150,35 @@ class PagoProvider extends ChangeNotifier {
     } finally {
       _isLoading = false;
       notifyListeners();
+    }
+  }
+
+  Future<void> searchCompra(BuildContext context, int idPago) async {
+    try {
+      _isLoading = true;
+      notifyListeners();
+
+      final response = await _apiClient.get('/api/v1/compra/$idPago');
+
+      print(response.statusCode);
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        if (data is List) {
+          _compra = {'items': data};
+        } else if (data is Map) {
+          _compra = Map<String, dynamic>.from(data);
+        }
+
+        print('Pago encontrado: $_compra');
+      } else {
+        throw Exception('Error al buscar la compra 1');
+      }
+    } catch (e) {
+      print(e.toString());
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error al buscar la compra: $e')),
+      );
     }
   }
 
